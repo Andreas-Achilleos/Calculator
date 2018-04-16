96ce6c10fb6585425f5d863746ee932730d9c09a
Andy Achilleos <andyachilleos@Andys-MacBook-Pro.local>
2018-04-16 16:29:37 +0100

    Different operators using switch statement

--- index.js
+++ index.js
@@ -4,11 +4,40 @@ console.log("Welcome to the Calculator!");
 
 console.log('\nPlease enter your first number: ');
 const argumentOne = readline.prompt();
-const numberOne = argumentOne;
+const numberOne = +argumentOne;
+
+console.log('\nPlease enter an operator: ');
+const operator = readline.prompt();
 
 console.log('\nPlease enter your second number: ');
 const argumentTwo = readline.prompt();
-const numberTwo = argumentTwo;
+const numberTwo = +argumentTwo;
 
-const answer = numberOne * numberTwo;
+var answer;
+
+// if (operator == "+") {
+//   answer = numberOne + numberTwo;
+// } else if (operator == "-") {
+//   answer = numberOne - numberTwo;
+// } else if (operator == "*") {
+//   answer = numberOne * numberTwo;
+// } else if (operator == "/") {
+//   answer = numberOne / numberTwo;
+// }
+
+switch (operator) {
+  case "+":
+    answer = numberOne + numberTwo;
+    break;
+  case "-":
+    answer = numberOne - numberTwo;
+    break;
+  case "*":
+    answer = numberOne * numberTwo;
+    break;
+  case "/":
+    answer = numberOne / numberTwo;
+    break;
+}
+
 console.log("\nThe answer is " + answer);
