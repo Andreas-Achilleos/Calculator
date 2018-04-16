33020b95b2008e771f316f1501dad54ac7c70a81
Andy Achilleos <andyachilleos@Andys-MacBook-Pro.local>
2018-04-16 16:03:23 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const readline = require('readline-sync');
+
 console.log("Welcome to the Calculator!");
+
+console.log('\nPlease enter your first number: ');
+const argumentOne = readline.prompt();
+const numberOne = argumentOne;
+
+console.log('\nPlease enter your second number: ');
+const argumentTwo = readline.prompt();
+const numberTwo = argumentTwo;
+
+const answer = numberOne * numberTwo;
+console.log("\nThe answer is " + answer);
